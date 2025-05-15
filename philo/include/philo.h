7b47ab30cba354b16ca51d84664c79d0bf2f61f7
Arthur Perez <artperez@c3r3p2.42perpignan.fr>
2025-05-15 13:14:23 +0200

    doing exec

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: artperez <artperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 09:55:46 by artperez          #+#    #+#             */
-/*   Updated: 2025/05/14 13:28:23 by artperez         ###   ########.fr       */
+/*   Updated: 2025/05/15 13:05:53 by artperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,33 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <sys/time.h>
 
 typedef struct s_fork
 {
     int fork_name;
-    
+	pthread_mutex_t lock;
 }   t_fork;
+
+typedef enum s_do 
+{
+    HAS_EAT,
+    HAS_SLEEP,
+} t_do;
 
 typedef struct s_philo
 {
     int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int nbr_of_must_eat;
     int philo_nbr;
-    struct s_fork   right_fork;
-    struct s_fork   left_fork;
+	pthread_t *thread;
+    t_do    has;
+	pthread_mutex_t lock;
+    struct s_fork   *right_fork;
+    struct s_fork   *left_fork;
 }   t_philo;
 
 // static size_t	ft_intlen(int n);
@@ -37,5 +51,8 @@ bool	check_input(char **inputs, int inputs_nbr);
 // static char	*gogotransfo(char *str, long n, size_t len);
 char	*ft_itoa(int n);
 int	ft_atoi(const char *str);
+void	init_threads(t_philo *philos, int philo_nbr);
+long long	get_time(struct timeval *time);
+void	*exec(void *data);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: artperez <artperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 09:54:56 by artperez          #+#    #+#             */
-/*   Updated: 2025/05/14 13:31:51 by artperez         ###   ########.fr       */
+/*   Updated: 2025/05/15 13:09:33 by artperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,19 +36,59 @@ bool	check_input(char **inputs, int inputs_nbr)
 			return (write(2, "Invalid Input : postive nbr required\n", 38),
 				(false));
 		i++;
+		j = 0;
 	}
 	return (true);
 }
 
-void	init_mutex(t_philo	*philos, int philo_nbr)
+void	init_threads(t_philo *philos, int philo_nbr)
 {
 	int	i;
-
+	
 	i = 0;
 	while (i <= philo_nbr)
 	{
-		
+		pthread_create(philos[i].thread, NULL, exec, &philos[i]);
+		i++;
 	}
+}
+
+void	*exec(void *data)
+{
+	t_philo *philo;
+	struct timeval	time;
+
+	philo = (t_philo*)data;
+	if (philo->has == HAS_SLEEP && philo->left_fork)
+	{
+		pthread_mutex_lock(&philo->left_fork->lock);
+		printf("%lld %i has taken a fork", get_time(&time), philo->philo_nbr);
+	}
+	if (philo->has == HAS_SLEEP && philo->right_fork)
+	{
+		pthread_mutex_lock(&philo->right_fork->lock);
+		printf("%lld %i has taken a fork", get_time(&time), philo->philo_nbr);
+	}
+	if (philo->has == HAS_SLEEP && philo->left_fork && philo->right_fork)
+	{
+		pthread_mutex_lock(&philo->lock);
+		printf("%lld %i is eating", get_time(&time), philo->philo_nbr);
+		usleep(philo->time_to_eat * 1000);
+		philo->has = HAS_EAT;
+		pthread_mutex_unlock(&philo->left_fork->lock);
+		pthread_mutex_unlock(&philo->right_fork->lock);
+		pthread_mutex_unlock(&philo->lock);
+	}
+	return (NULL);
+}
+
+long long	get_time(struct timeval *time)
+{
+	long long	res;
+
+	gettimeofday(time, NULL);
+	res = time->tv_sec * 1000 + time->tv_usec / 1000;
+	return (res);
 }
 
 t_philo	*init_philo(char **inputs)
@@ -58,17 +98,23 @@ t_philo	*init_philo(char **inputs)
 	t_philo	*philos;
 	t_fork	*forks;
 
-	i = 1;
+	i = 0;
 	philo_nbr = ft_atoi(inputs[1]);
 	philos = malloc(sizeof(t_philo) * ft_atoi(inputs[1]));
 	forks = malloc(sizeof(t_philo) * ft_atoi(inputs[1]));
 	while (i < philo_nbr)
 	{
-		forks[i].fork_name = i;
+		forks[i].fork_name = i + 1;
+		philos[i].philo_nbr = i + 1;
 		philos[i].time_to_die = ft_atoi(inputs[2]);
-		philos[i].philo_nbr = i;
-		philos[i].left_fork = forks[i];
-		philos[i].right_fork = forks[i + 1];
+		philos[i].time_to_eat = ft_atoi(inputs[3]);
+		philos[i].time_to_sleep = ft_atoi(inputs[4]);
+		if (inputs[5])
+			philos[i].nbr_of_must_eat = ft_atoi(inputs[5]);
+		philos[i].left_fork = &forks[i];
+		philos[i].right_fork = &forks[i + 1];
+		philos[i].has = HAS_SLEEP;
+		i++;
 	}
 	return (philos);
 }
@@ -80,6 +126,6 @@ int	main(int argc, char **argv)
 	if (!check_input(argv, argc))
 		return (-1);
 	philos = init_philo(argv);
-	init_mutex(philos, ft_atoi(argv[1]));
+	init_threads(philos, ft_atoi(argv[1]));
 	return (0);
 }
